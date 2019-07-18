8f8669539f5bcfdc326d3623575255944fabc118
Oswald <oskari.tiala@haltu.fi>
2019-07-18 16:38:30 +0300

    Fix a bug with HobbySerializer returning hobby name instead of location name

--- src/harrastuspassi/harrastuspassi/serializers.py
+++ src/harrastuspassi/harrastuspassi/serializers.py
@@ -1,9 +1,9 @@
 from rest_framework import serializers
-from harrastuspassi.models import Hobby
+from harrastuspassi.models import Hobby, Location
 
 
 class HobbySerializer(serializers.ModelSerializer):
-  location = serializers.CharField(source='name')
+  location = serializers.StringRelatedField()
   day_of_week = serializers.CharField(source='get_day_of_week_display')
 
   class Meta:
