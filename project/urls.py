cb7b1cdda6a68d01022ce38b94ea556701d9259a
Oskari Tiala <oskari.tiala@haltu.fi>
2019-07-30 13:13:02 +0300

    Add schema view

--- project/urls.py
+++ project/urls.py
@@ -1,17 +1,22 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import include, url
+from django.urls import include, path
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
 from project import settings
 
 admin_urls = [
-  url(r'^sysadmin/', admin.site.urls),
+  path('sysadmin/', admin.site.urls),
 ]
 
 app_urls = [
-  url(r'', include('harrastuspassi.urls')),
+  path('', include('harrastuspassi.urls')),
+  path('openapi', get_schema_view(
+    title="Harrastuspassi",
+    description="API documentation"
+  ), name='openapi-schema'),
 ]
 
 static_urls = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
