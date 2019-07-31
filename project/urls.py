9faba812718462f569c0a674f8eab8ed3237c603
Oskari Tiala <oskari.tiala@haltu.fi>
2019-07-31 13:13:26 +0300

    Initialize API documentation
    - Generate dynamic schema with DRF's get_schema_view
    - Use ReDoc user interface for schema
    - Show internal /mobile-api/ documentation only when DEBUG is set to True

--- project/urls.py
+++ project/urls.py
@@ -3,9 +3,16 @@
 from django.urls import include, path
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from rest_framework.schemas import get_schema_view
-from project import settings
+
+from harrastuspassi.urls import public_urlpatterns, internal_urlpatterns
+
+schema_url_patterns = public_urlpatterns
+if settings.DEBUG:
+  schema_url_patterns += internal_urlpatterns
 
 admin_urls = [
   path('sysadmin/', admin.site.urls),
@@ -15,8 +22,13 @@ app_urls = [
   path('', include('harrastuspassi.urls')),
   path('openapi', get_schema_view(
     title="Harrastuspassi",
-    description="API documentation"
+    description="API documentation",
+    patterns=schema_url_patterns
   ), name='openapi-schema'),
+  path('redoc/', TemplateView.as_view(
+    template_name='redoc.html',
+    extra_context={'schema_url': 'openapi-schema'}
+  ), name='redoc'),
 ]
 
 static_urls = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
