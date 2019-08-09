571104a9ebe8fb58320e5c6abc4e7eb7cfa7f77e
Jussi Heikkilä <jussi.heikkila@haltu.fi>
2019-08-09 16:24:37 +0300

    HobbyCategory endpoint can include data of child categories
    Use include=child_categories to include full child category data. Use
    this only when accessing a single category through a detail enpoint or
    when filtering the list of categories to include only root categories.
    Otherwise multiples of categories are returned, as each category will be
    listed with their children.

--- src/harrastuspassi/harrastuspassi/tests/test_hobby_categories_api.py
+++ src/harrastuspassi/harrastuspassi/tests/test_hobby_categories_api.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from harrastuspassi.models import HobbyCategory
 
 
 @pytest.mark.django_db
@@ -10,3 +11,16 @@ def test_hobby_category_exists(api_client, test_hobby_category):
     assert response.status_code == 200
     assert len(response.data) == 1
     assert response.data[0]['name'] == 'Urheilu'
+
+
+@pytest.mark.django_db
+def test_hobby_category_include_children(api_client, test_hobby_category):
+    child = HobbyCategory.objects.create(name="child", parent=test_hobby_category)
+    grand_child = HobbyCategory.objects.create(name="grand_child", parent=child)
+    api_url = reverse('hobbycategory-detail', kwargs={'pk': test_hobby_category.pk})
+    url = f"{api_url}?include=child_categories"
+    response = api_client.get(url)
+    assert response.data['id'] == test_hobby_category.pk
+    assert response.data['child_categories'][0]['id'] == child.pk
+    assert response.data['child_categories'][0]['child_categories'][0]['id'] == grand_child.pk
+
