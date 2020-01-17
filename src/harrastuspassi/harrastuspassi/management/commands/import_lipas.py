7cd5516f124ea8bdee71a9486521a52651dccd48
Oskari Tiala <oskari.tiala@haltu.fi>
2020-01-17 11:49:35 +0200

    Improve import script based on feedback

--- src/harrastuspassi/harrastuspassi/management/commands/import_lipas.py
+++ src/harrastuspassi/harrastuspassi/management/commands/import_lipas.py
@@ -53,10 +53,24 @@ class Command(BaseCommand):
             self.session = session
 
             while True:
+                query_params = {
+                    'fields': [
+                        'type.name',
+                        'name',
+                        'freeUse',
+                        'location.city.name',
+                        'location.postalCode',
+                        'location.postalOffice',
+                        'location.coordinates.wgs84',
+                        'location.address',
+                        'type.typeCode',
+                    ],
+                    'page': page_number,
+                }
                 response = self.session.get(
-                    options['url']
-                    + f'?fields=type.name&fields=name&fields=freeUse&fields=location.city.name&fields=location.postalCode&fields=location.postalOffice&fields=location.coordinates.wgs84&fields=location.address&fields=type.typeCode&page={page_number}',
-                    timeout=REQUESTS_TIMEOUT
+                    options['url'],
+                    timeout=REQUESTS_TIMEOUT,
+                    params=query_params,
                 )
                 has_results = response.text != '[]'
                 if not has_results:
@@ -64,64 +78,66 @@ class Command(BaseCommand):
                 response.raise_for_status()
                 response_json = response.json()
                 for sports_place in response_json:
-                    zip_code_is_valid = 'postalCode' in sports_place['location'] and len(sports_place['location']['postalCode'].strip()) == 5
+                    cleaned_postal_code = sports_place['location'].get('postalCode', '').strip()
+                    zip_code_is_valid = 'postalCode' in sports_place['location'] and len(cleaned_postal_code) == 5
                     is_free = 'freeUse' in sports_place and sports_place['freeUse']
 
-                    if 'coordinates' in sports_place['location']:
-                        name = sports_place.get('name')
-                        type_code = sports_place['type'].get('typeCode')
+                    if not 'coordinates' in sports_place['location']:
+                        continue
+                    name = sports_place.get('name')
+                    type_code = sports_place['type'].get('typeCode')
 
-                        self.stdout.write(f'Handling hobby: {name}')
-                        if zip_code_is_valid and is_free and type_code in self.category_mappings.keys():
-
-                            location, location_created = Location.objects.update_or_create(
-                                data_source=self.source,
-                                name=sports_place.get('name'),
-                                defaults={
-                                    'address': sports_place['location'].get('address', ''),
-                                    'zip_code': sports_place['location'].get('postalCode', '').strip(),
-                                    'city': sports_place['location']['city'].get('name'),
-                                    'coordinates': Point(
-                                        sports_place['location']['coordinates']['wgs84'].get('lon', ''),
-                                        sports_place['location']['coordinates']['wgs84'].get('lat', '')
-                                    )
-                                }
+                    self.stdout.write(f'Handling hobby: {name}')
+                    if not zip_code_is_valid or not is_free or not type_code in self.category_mappings.keys():
+                        continue
+                    location, location_created = Location.objects.update_or_create(
+                        data_source=self.source,
+                        name=sports_place.get('name'),
+                        zip_code= cleaned_postal_code,
+                        defaults={
+                            'address': sports_place['location'].get('address', ''),
+                            'city': sports_place['location']['city'].get('name'),
+                            'coordinates': Point(
+                                sports_place['location']['coordinates']['wgs84'].get('lon', ''),
+                                sports_place['location']['coordinates']['wgs84'].get('lat', '')
                             )
+                        }
+                    )
 
-                            if location_created:
-                                created_locations += 1
-                            else:
-                                updated_locations += 1
+                    if location_created:
+                        created_locations += 1
+                    else:
+                        updated_locations += 1
 
-                            category, _category_created = HobbyCategory.objects.get_or_create(
-                                name=self.category_mappings[type_code],
-                            )
+                    category, _category_created = HobbyCategory.objects.get_or_create(
+                        name=self.category_mappings[type_code],
+                    )
 
-                            hobby, hobby_created = Hobby.objects.update_or_create(
-                                data_source=self.source,
-                                name=sports_place['name'],
-                                price_type=Hobby.TYPE_FREE,
-                                defaults={
-                                    'location': location,
-                                }
-                            )
-                            hobby.categories.set([category])
+                    hobby, hobby_created = Hobby.objects.update_or_create(
+                        data_source=self.source,
+                        name=sports_place['name'],
+                        price_type=Hobby.TYPE_FREE,
+                        defaults={
+                            'location': location,
+                        }
+                    )
+                    hobby.categories.set([category])
 
-                            hobbyevent, hobbyevent_created = HobbyEvent.objects.update_or_create(
-                                data_source=self.source,
-                                hobby=hobby,
-                                defaults={
-                                    'start_date': datetime.date.today(),
-                                    'end_date': datetime.date.today() + datetime.timedelta(days=365),
-                                    'start_time': datetime.datetime.strptime('00:00', '%H:%M').time(),
-                                    'end_time': datetime.datetime.strptime('00:00', '%H:%M').time(),
-                                }
-                            )
+                    hobbyevent, hobbyevent_created = HobbyEvent.objects.update_or_create(
+                        data_source=self.source,
+                        hobby=hobby,
+                        defaults={
+                            'start_date': datetime.date.today(),
+                            'end_date': datetime.date.today() + datetime.timedelta(days=365),
+                            'start_time': datetime.datetime.strptime('00:00', '%H:%M').time(),
+                            'end_time': datetime.datetime.strptime('00:00', '%H:%M').time(),
+                        }
+                    )
 
-                            if hobby_created:
-                                created_hobbies += 1
-                            else:
-                                updated_hobbies += 1
+                    if hobby_created:
+                        created_hobbies += 1
+                    else:
+                        updated_hobbies += 1
                 page_number += 1
-            execution_time = round(time.time() - start_time, 2)
-            self.stdout.write(f'\n{created_hobbies} new hobbies, {updated_hobbies} updated hobbies, {created_hobbies} new locations, {updated_hobbies} updated locations in {execution_time} seconds')
+        execution_time = round(time.time() - start_time, 2)
+        self.stdout.write(f'\n{created_hobbies} new hobbies, {updated_hobbies} updated hobbies, {created_hobbies} new locations, {updated_hobbies} updated locations in {execution_time} seconds')
