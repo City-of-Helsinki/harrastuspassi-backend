15cf5ab07c71d4bc005c741de5587b68c0be5019
Daniel Kislyuk <daniel.kislyuk@gmail.com>
2020-08-26 09:41:37 +0300

    fix timezone treatment

--- src/harrastuspassi/harrastuspassi/settings.py
+++ src/harrastuspassi/harrastuspassi/settings.py
@@ -22,3 +22,5 @@ LIPPUPISTE_URL = getattr(settings, 'HARRASTUSPASSI_LIPPPUPISTE_URL',
 #  Excluded: yso:p4354 - lapset, yso:p13050 - lapsiperheet, yso:p16485 - peruskoululaiset, yso:p20513 - vauvaperheet
 LINKEDEVENTS_URL = getattr(settings, 'HARRASTUSPASSI_LINKEDEVENTS_URL',
                            'https://api.hel.fi/linkedevents/v1/event/?keyword=yso:p11617,yso:p16486&keyword!=yso:p4354,yso:p13050,yso:p16485,yso:p20513')
+
+TIME_ZONE = getattr(settings, 'TIME_ZONE', 'Europe/Helsinki')
