951717b4a4025cbe5477313d6155aa370e5b961e
Oskari Tiala <oskari.tiala@haltu.fi>
2020-08-31 13:53:11 +0300

    Add comments to next_event update script and improve output

--- src/harrastuspassi/harrastuspassi/management/commands/update_hobby_next_event.py
+++ src/harrastuspassi/harrastuspassi/management/commands/update_hobby_next_event.py
@@ -6,8 +6,10 @@ from harrastuspassi.models import Hobby, HobbyEvent
 
 
 class Command(BaseCommand):
+    """ Update next event for each Hobby """
     def handle(self, *args, **options):
-        start_date_in_future_count = Hobby.objects.update(
+        # Prefer upcoming events over ongoing events
+        updated_count = Hobby.objects.update(
             next_event_id=models.Subquery(
                 HobbyEvent.objects.filter(
                     hobby=models.OuterRef('pk'),
@@ -17,7 +19,10 @@ class Command(BaseCommand):
                 .order_by('start_date')[:1]
             )
         )
-        end_date_in_future_count = Hobby.objects.filter(next_event_id__isnull=True).update(
+        self.stdout.write(f'Updated {updated_count} hobbies')
+        # If there are Hobbies with only ongoing events and no upcoming events
+        # then fall back to the ongoing event
+        fallback_updated_count = Hobby.objects.filter(next_event_id__isnull=True).update(
             next_event_id=models.Subquery(
                 HobbyEvent.objects.filter(
                     hobby=models.OuterRef('pk'),
@@ -27,3 +32,10 @@ class Command(BaseCommand):
                 .order_by('start_date')[:1]
             )
         )
+        self.stdout.write(f'Updated {fallback_updated_count} hobbies with fallback query')
+        hobbies_without_next_event_count = Hobby.objects.filter(next_event_id__isnull=True).count()
+        start_date_in_future_count = updated_count - fallback_updated_count
+        end_date_in_future_count = fallback_updated_count - hobbies_without_next_event_count
+        self.stdout.write(f'{start_date_in_future_count} hobbies with upcoming next event')
+        self.stdout.write(f'{end_date_in_future_count} hobbies with ongoing next event')
+        self.stdout.write(f'{hobbies_without_next_event_count} hobbies without next event')
